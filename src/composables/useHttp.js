902a1a3a4763f66e1f3f27a31258ca4c34cffe9c
iocuc <648795318@qq.com>
2024-12-10 13:45:17 +0800

    .

--- src/composables/useHttp.js
+++ src/composables/useHttp.js
@@ -4,31 +4,17 @@ export default async function useHttp(method, url, params) {
     const reqMethod = method.toUpperCase();
 
     if (reqMethod === 'POST') {
-      response = await fetch(url, { method: reqMethod, body: params });
+      response = await fetch(url, { method: reqMethod, body: params, credentials: 'include' });
     } else if (reqMethod === 'GET') {
       const urlParams = new URLSearchParams(params);
       const fullUrl = `${url}?${urlParams.toString()}`;
-      response = await fetch(fullUrl, { method: reqMethod });
-    }
-
-    if (!response) {
+      response = await fetch(fullUrl, { method: reqMethod, credentials: 'include' });
+    } else {
       throw new Error('不支持的方法');
-    }
-
-    if (!response.ok) {
-      throw new Error(response);
-    }
-
-    // 获取 Set-Cookie 头部
-    const setCookieHeader = response.headers.get('Set-Cookie');
-    if (setCookieHeader) {
-      // 将 Cookie 存入浏览器
-      document.cookie = setCookieHeader.split(';')[0]; // 只取第一个 Cookie
-      console.log('Cookie 已存入浏览器:', document.cookie);
     }
 
     return await response.json();
   } catch (error) {
-    console.error('发生错误:', error);
+    throw new Error(error);
   }
 }
